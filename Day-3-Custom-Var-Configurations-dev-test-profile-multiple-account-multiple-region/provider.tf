e84e2a1906e877b1e8cb0e959076bce4dfb92acb
Yogeshwar Walmik Deshmukh <deshmukh.yogesh333@gmail.com>
2026-03-14 18:35:49 +0530

    Update provider.tf

--- Day-3-Custom-Var-Configurations-dev-test-profile-multiple-account-multiple-region/provider.tf
+++ Day-3-Custom-Var-Configurations-dev-test-profile-multiple-account-multiple-region/provider.tf
@@ -6,14 +6,25 @@
 # Profile "test" → us-west-2  (test environment)
 # ============================================================
 
+#defualt region is us-east-1 but we can specify the region here if we want to use a different region
+ #here dev = defualt profile in provider.tf
+
+# calling keys from my local machine dev profile credentials 
+## dev environment provider block##
+
 provider "aws" {
-  alias   = "devenv"
+  alias   = "devenv" #if multiple providers are used then we have to give alias to the provider and then we can call that provider in the resource block
   region  = "us-east-1"
   profile = "dev"
 }
 
+# calling keys from my local machine prod profile credentials 
+## prod environment provider block##
 provider "aws" {
-  alias   = "testenv"
+  alias   = "testenv" 
   region  = "us-west-2"
   profile = "test"
 }
+
+
+
