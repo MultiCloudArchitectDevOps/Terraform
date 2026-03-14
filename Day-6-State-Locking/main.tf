2a9b924d3a675863f384123d98910a0762ca0f10
yogesh <deshmukh.yogesh333@gmail.com>
2026-03-14 17:36:20 +0530

    state

--- Day-6-State-Locking/main.tf
+++ Day-6-State-Locking/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "name" {
 
 resource "aws_subnet" "name" {
   vpc_id = aws_vpc.name.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/16"
   tags = {
     Name = "subnet"
   }
