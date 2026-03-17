62f5a87a4e357024e2eb24153f9c951efec0fda2
yogesh <deshmukh.yogesh333@gmail.com>
2026-03-17 23:43:02 +0530

    final

--- Day-7-custom-infra-landingzone/main.tf
+++ Day-7-custom-infra-landingzone/main.tf
@@ -2,6 +2,7 @@
 # Public Route Table, Associate Public Subnet, Private Route Table, Associate Private Subnet,
 # EC2 Instance, and Security Group
 
+#VPC
 resource "aws_vpc" "prod" {
     cidr_block = "10.0.0.0/16"
     tags = {
@@ -10,8 +11,8 @@ resource "aws_vpc" "prod" {
     }
 }
 
-#public subnet
-resource "aws_subnet" "public" {
+#Public Subnet
+resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.prod.id
     cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1a"
@@ -20,8 +21,8 @@ resource "aws_subnet" "public" {
     }
 }
 
-#private subnet
-resource "aws_subnet" "private" {
+#Private Subnet
+resource "aws_subnet" "private_subnet" {
     vpc_id = aws_vpc.prod.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "us-east-1a"
@@ -30,6 +31,7 @@ resource "aws_subnet" "private" {
     }
 }
 
+#Internate Gateway
 resource "aws_internet_gateway" "prod" {
     vpc_id = aws_vpc.prod.id
     tags = {
@@ -37,26 +39,69 @@ resource "aws_internet_gateway" "prod" {
     }
 }
 
-resource "aws_route_table" "prod" {
-  vpc_id = aws_vpc.prod.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.prod.id
-
+# Elastic IP for NAT 
+resource "aws_eip" "nat_eip" {
+    domain = "vpc"
+    tags = {
+      Name = "Elastic IP"
     }
 }
 
-resource "aws_route_table_association" "prod" {
-    subnet_id = aws_subnet.public.id
-    route_table_id = aws_route_table.prod.id
+# NAT Gateway
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "Main-NAT-Gateway"
+  }
+
+  depends_on = [aws_internet_gateway.prod]
 }
 
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.prod.id
+  tags = {
+    Name = "Public Route Table"
+  }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.prod.id
+}
+}
+
+# Associate Public Subnet
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.prod.id
+ tags = {
+    Name = "Private Route Table"
+  }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+}
+
+# Associate Private Subnet
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+#Security Group
 resource "aws_security_group" "prod" {
     name = "prod-sg"
     description = "Allow SSH and HTTP traffic"
     vpc_id = aws_vpc.prod.id
     
-    #inbound rule to allow SSH traffic
+    #Inbound rule to allow SSH traffic
     ingress {
         from_port = 22
         to_port = 22
@@ -71,7 +116,7 @@ resource "aws_security_group" "prod" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    #outbound rule to allow all traffic
+    #Outbound rule to allow all traffic
     egress {
         from_port = 0
         to_port = 0
@@ -80,10 +125,11 @@ resource "aws_security_group" "prod" {
     }
 }
 
+#EC2 Instance
 resource "aws_instance" "prod" {
     ami = "ami-02dfbd4ff395f2a1b"
     instance_type = "t2.micro"
-    subnet_id = aws_subnet.public.id
+    subnet_id = aws_subnet.public_subnet.id
     vpc_security_group_ids = [aws_security_group.prod.id]
     tags = {
         Name = "EC2-Instance"
