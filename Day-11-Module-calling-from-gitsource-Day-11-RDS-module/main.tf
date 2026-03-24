b7d58cd13aa392417f0edf161427df763fa930c3
yogesh <deshmukh.yogesh333@gmail.com>
2026-03-24 13:35:07 +0530

    day-11

--- Day-11-Module-calling-from-gitsource-Day-11-RDS-module/main.tf
+++ Day-11-Module-calling-from-gitsource-Day-11-RDS-module/main.tf
@@ -1,46 +1,32 @@
-module "db" {
-  source = "terraform-aws-modules/rds/aws"
 
-  identifier = "demodb"
+module "rds" {
+    source = "github.com/MultiCloudArchitectDevOps/Terraform/tree/main/Day-11-RDS-module"
+    #user your github source
+    vpc_cidr = "10.0.0.0/16"
 
-  engine            = "mysql"
-  engine_version    = "8.0"
-  instance_class    = "db.t3.micro"
-  allocated_storage = 5
-
-  db_name  = "demodbbb"
-  username = "user"
-  port     = "3306"
-
-  #iam_database_authentication_enabled = true
-   manage_master_user_password = true
-
-  vpc_security_group_ids = ["sg-0bebc6bd7410e6882"]
-
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
-
-  # Enhanced Monitoring - see example for details on how to create the role
-  # by yourself, in case you don't want to create it automatically
-  #monitoring_interval    = "30"
-  #monitoring_role_name   = "MyRDSMonitoringRole"
-  #create_monitoring_role = true
-
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
+subnets = {
+  subnet1 = {
+    cidr = "10.0.0.0/24"
+    az   = "us-east-1a"
   }
+  subnet2 = {
+    cidr = "10.0.1.0/24"
+    az   = "us-east-1b"
+  }
+}
 
-  # DB subnet group
-  create_db_subnet_group = true
-  subnet_ids             = ["subnet-01758d52b48c67d4e", "subnet-0cb1bc154bbea15fb"]
+db_identifier         = "rds-test"
+db_name               = "mydb"
+db_instance_class     = "db.t3.micro"
+db_allocated_storage  = var.db_allocated_storage
+db_username           = var.db_username
 
-  # DB parameter group
-  family = "mysql8.0"
+#note here db_username is passing value to module variable and var.db_username is decalrined in variables.tf file and 
+#value is assigned in terraform.tfvars file
 
-  # DB option group
-  major_engine_version = "8.0"
 
-  # Database Deletion Protection
-  deletion_protection = true 
+backup_window      = "02:00-03:00"
+maintenance_window = "sun:04:00-sun:05:00"
+
+  
 }
