8d7e8360e3636cb10d2c9ebe619599833b8f9c35
yogesh <deshmukh.yogesh333@gmail.com>
2026-03-24 13:51:19 +0530

    day-11 updated

--- Day-11-Module-calling-from-gitsource-Day-11-RDS-module/main.tf
+++ Day-11-Module-calling-from-gitsource-Day-11-RDS-module/main.tf
@@ -1,6 +1,8 @@
 
+
 module "rds" {
-    source = "github.com/MultiCloudArchitectDevOps/Terraform/tree/main/Day-11-RDS-module"
+    
+    source = "github.com/MultiCloudArchitectDevOps/Terraform/Day-11-RDS-module"
     #user your github source
     vpc_cidr = "10.0.0.0/16"
 
@@ -21,8 +23,7 @@ db_instance_class     = "db.t3.micro"
 db_allocated_storage  = var.db_allocated_storage
 db_username           = var.db_username
 
-#note here db_username is passing value to module variable and var.db_username is decalrined in variables.tf file and 
-#value is assigned in terraform.tfvars file
+#note here db_username is passing value to module variable and var.db_username is decalrined in variables.tf file and value is assigned in terraform.tfvars file
 
 
 backup_window      = "02:00-03:00"
